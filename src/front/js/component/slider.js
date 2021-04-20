fe10cb6a2b05870bce1f8d0687a8ced5a25d6d56
LizzH89 <lizziehidalgoleiva@gmail.com>
2021-04-20 01:59:24 +0000

    Agragando titulos

--- src/front/js/component/slider.js
+++ src/front/js/component/slider.js
@@ -11,22 +11,22 @@ export const Slider = () => {
 				<Carousel.Item interval={1000}>
 					<img className="d-block w-100" src={slider1} alt="First slide" fluid />
 					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						<h3>Nuevas Oportunidades</h3>
+						<p>¡Investiga carreras y vuelve a capacitarte!</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img className="d-block w-100" src={slider2} alt="Second slide" />
 					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<h3>Carreras en Tecnología </h3>
+						<p>Desarrolla nuevas habilidades digitales.</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img className="d-block w-100" src={slider3} alt="Third slide" />
 					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						<h3>Trabajadores Mayores de 45 Años</h3>
+						<p>Aproximadamente el 27 % de los trabajadores de 45 años o más trabajaban tiempo parcial.</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
