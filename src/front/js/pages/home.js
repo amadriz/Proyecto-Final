28d684f9d1069b894934476e0f88fcdb898dfc59
Allan Madriz <ahovenga@gmail.com>
2021-05-13 17:38:57 +0000

    Nueva seccion con efecto parallax

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,6 +4,10 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import { Container, Row, Col, Jumbotron } from "react-bootstrap";
 import arteTipsCurri from "../../img/arteTipsCurri.png";
 import { Slider } from "../component/slider";
+import { Parallax } from "react-parallax";
+
+import parallaxBg from "../../img/parallaxBg.jpg";
+
 import logo1 from "../../img/logo1.png";
 import logo2 from "../../img/logo2.png";
 import logo3 from "../../img/logo3.png";
@@ -11,6 +15,15 @@ import logo4 from "../../img/logo4.png";
 import logo5 from "../../img/logo5.png";
 import logo6 from "../../img/logo6.png";
 import logo7 from "../../img/logo7.png";
+
+const inlineStyle = {
+	background: "#fff",
+	left: "50%",
+	top: "50%",
+	position: "absolute",
+	padding: "20px",
+	transform: "tranlate(-50%, -50%)"
+};
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -44,6 +57,72 @@ export const Home = () => {
 						</p>
 					</Col>
 				</Row>
+			</Container>
+			{/* PARALLAX START */}
+			<Row className="mt-3 text-light">
+				<Col sm={12}>
+					<Parallax bgImage={parallaxBg} strength={500}>
+						<div style={{ height: 600 }}>
+							<Row className="mb-3">
+								<Col className="p-3 text-uppercase text-light" sm={12}>
+									<h2 className="text-center mt-5">Beneficios trabajadores:</h2>
+								</Col>
+								<Col sm={6} className="p-4">
+									<Col sm={12}>
+										<h4 className="text-center">Red de contactos:</h4>
+										<h6>
+											Debido a su larga trayectoria profesional, suelen poseer una agenda muy
+											completa en diferentes sectores y cargos, lo que puede resultar muy
+											provechoso para abrir puertas de modo más rápido para cualquier empresa.
+										</h6>
+									</Col>
+									<Col sm={12} className="p-2">
+										<h4 className="text-center">Mayor lealtad:</h4>
+										<h6>
+											Dado que suelen tener cumplidas gran parte de sus expectativas personales y
+											profesionales no toman sus decisiones ni en base a la remuneración que
+											reciben ni al puesto que ostentan, sino, sobre todo, a la satisfacción y el
+											clima laboral.
+										</h6>
+									</Col>
+									<Col sm={12} className="p-2">
+										<h4 className="text-center">Experiencia y conocimiento:</h4>
+										<h6>
+											Pueden convertirse en grandes aliados en cualquier proceso de negociación
+											así como en la resolución de una situación de conflicto, siendo
+											habitualmente más calmados a la hora de expresar sus opiniones e
+											inquietudes.
+										</h6>
+									</Col>
+								</Col>
+								{/* SEGUNDA COLUMNA DE BENEFICIOS */}
+								<Col sm={6} className="p-4">
+									<Col sm={12} className="p-2">
+										<h4 className="text-center">Gran motivación:</h4>
+										<h6>
+											Una parte relevante de estos trabajadores han pasado un periodo de tiempo
+											prolongado en el desempleo por lo que valoran sobremanera cualquier nueva
+											oportunidad profesional que se les presente, dando lo mejor de sí mismos.
+										</h6>
+									</Col>
+									<Col sm={12} className="p-2">
+										<h4 className="text-center">Valor y compromiso:</h4>
+										<h6>
+											La gran mayoría de estas personas pertenece a la generación de los
+											babyboomers, y combinan una gran preparación profesional con unos valores
+											muy tradicionales (fidelidad, compromiso). Elementos clave para cualquier
+											pyme.
+										</h6>
+									</Col>
+								</Col>
+							</Row>
+						</div>
+					</Parallax>
+				</Col>
+			</Row>
+			{/* PARALLAX ENDS */}
+
+			<Container fluid className="mb-5">
 				<Row className="mt-5">
 					<Col sm={12}>
 						<h1
